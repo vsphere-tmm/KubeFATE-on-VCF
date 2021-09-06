a58f8409a78d7c5b54dc3aa865d7df7f51eb29e7
ytsophie <ytsophie@gmail.com>
2021-09-05 23:46:27 -0700

    add notes
    add notes for example usecase

--- credit_card_use_case/splitting_data.sh
+++ credit_card_use_case/splitting_data.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-
-# create guest data 
-head -1 creditcard.csv | awk '{print "\"id\"," $0}' | tr -d \" > creditcard_guest.csv
-awk 'FNR>1 && FNR%2==0 {print NR-1","$1}'  creditcard.csv  | tr -d \" >> creditcard_guest.csv
-
-# create host data
-head -1 creditcard.csv | awk '{print "\"id\"," $0}' | tr -d \" > creditcard_host.csv
-awk 'FNR>1 && FNR%2==1 {print NR-1","$1}'  creditcard.csv | tr -d \" >> creditcard_host.csv
+head -1 creditcard.csv | tr -d \" > creditcard_guest.csv
+awk 'FNR>1 && FNR%2==0 {print $1}'  creditcard.csv  | tr -d \" >> creditcard_guest.csv
+head -1 creditcard.csv | tr -d \" > creditcard_host.csv
+awk 'FNR>1 && FNR%2==1 {print $1}'  creditcard.csv | tr -d \" >> creditcard_host.csv
